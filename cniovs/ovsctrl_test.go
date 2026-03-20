david-marchand/userspace-cni-network-plugin

commit 984c2750ee83fc3509d992c111f1bde299cf56be

cniovs: Add QoS/rate-limiting for vhost-user ports
Add bandwidth rate-limiting support for OVS-DPDK vhost-user ports using
the same parameters as the bandwidth CNI plugin:
- ingressRate/ingressBurst: limit traffic TO the container
- egressRate/egressBurst: limit traffic FROM the container

These map to OVS-DPDK QoS mechanisms:
- Container ingress uses OVS egress-policer (cir/cbs in bytes)
- Container egress uses OVS ingress_policing_rate/burst (in kbps/kb)

Configuration is added to the host section:
  "bandwidth": {
    "ingressRate": 1000000000,   // bits per second
    "ingressBurst": 1000000000,  // bits
    "egressRate": 1000000000,
    "egressBurst": 1000000000
  }

Rate and burst must both be set for each direction (validated at config
load time). QoS application is best-effort - failures are logged but do
not prevent pod creation. QoS settings are cleared before port deletion.

Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>

cniovs/ovsctrl.go
@@ -216,3 +216,72 @@ func doesBridgeContainInterfaces(bridge_name string) bool {
 
 	return found
 }
+
+// setIngressPolicing sets ingress policing on an OVS interface to limit
+// traffic FROM the container (container egress). Rate is in bits/sec, burst is in bits.
+// OVS uses kbps for rate and kb for burst.
+func setIngressPolicing(portName string, rateBits uint64, burstBits uint64) error {
+	// Convert bits/sec to kbps and bits to kb
+	rateKbps := rateBits / 1000
+	burstKb := burstBits / 1000
+
+	// COMMAND: ovs-vsctl set Interface <port> ingress_policing_rate=<kbps> ingress_policing_burst=<kb>
+	cmd := "ovs-vsctl"
+	args := []string{
+		"set", "Interface", portName,
+		fmt.Sprintf("ingress_policing_rate=%d", rateKbps),
+		fmt.Sprintf("ingress_policing_burst=%d", burstKb),
+	}
+	_, err := execCommand(cmd, args)
+	logging.Verbosef("ovsctl.setIngressPolicing(): port=%s rate=%d kbps burst=%d kb return=%v",
+		portName, rateKbps, burstKb, err)
+	return err
+}
+
+// clearIngressPolicing removes ingress policing from an OVS interface.
+func clearIngressPolicing(portName string) error {
+	// COMMAND: ovs-vsctl set Interface <port> ingress_policing_rate=0 ingress_policing_burst=0
+	cmd := "ovs-vsctl"
+	args := []string{
+		"set", "Interface", portName,
+		"ingress_policing_rate=0",
+		"ingress_policing_burst=0",
+	}
+	_, err := execCommand(cmd, args)
+	logging.Verbosef("ovsctl.clearIngressPolicing(): port=%s return=%v", portName, err)
+	return err
+}
+
+// setEgressPolicer sets an egress-policer QoS on an OVS port to limit
+// traffic TO the container (container ingress). Rate is in bits/sec, burst is in bits.
+// OVS egress-policer uses bytes/sec for cir and bytes for cbs.
+func setEgressPolicer(portName string, rateBits uint64, burstBits uint64) error {
+	// Convert bits to bytes
+	rateBytes := rateBits / 8
+	burstBytes := burstBits / 8
+
+	// COMMAND: ovs-vsctl set port <port> qos=@newqos -- \
+	//   --id=@newqos create qos type=egress-policer other-config:cir=<bytes/sec> other-config:cbs=<bytes>
+	cmd := "ovs-vsctl"
+	args := []string{
+		"set", "port", portName, "qos=@newqos", "--",
+		"--id=@newqos", "create", "qos", "type=egress-policer",
+		fmt.Sprintf("other-config:cir=%d", rateBytes),
+		fmt.Sprintf("other-config:cbs=%d", burstBytes),
+	}
+	_, err := execCommand(cmd, args)
+	logging.Verbosef("ovsctl.setEgressPolicer(): port=%s cir=%d bytes/sec cbs=%d bytes return=%v",
+		portName, rateBytes, burstBytes, err)
+	return err
+}
+
+// clearEgressPolicer removes egress-policer QoS from an OVS port.
+func clearEgressPolicer(portName string) error {
+	// COMMAND: ovs-vsctl clear port <port> qos
+	// Note: This clears the QoS reference. Orphaned QoS objects can be garbage collected.
+	cmd := "ovs-vsctl"
+	args := []string{"--if-exists", "clear", "port", portName, "qos"}
+	_, err := execCommand(cmd, args)
+	logging.Verbosef("ovsctl.clearEgressPolicer(): port=%s return=%v", portName, err)
+	return err
+}

cniovs/ovsctrl_test.go
@@ -444,3 +444,137 @@ func TestExecCommand(t *testing.T) {
 		assert.Equal(t, expOut, out, "Unexpected result")
 	})
 }
+
+func TestSetIngressPolicing(t *testing.T) {
+	expCmd := "ovs-vsctl"
+	portName := "test-port"
+	rateBits := uint64(1000000000)  // 1 Gbps
+	burstBits := uint64(1000000000) // 1 Gb
+	// Expected conversion: bits/sec -> kbps, bits -> kb
+	expArgs := []string{"set", "Interface", "test-port", "ingress_policing_rate=1000000", "ingress_policing_burst=1000000"}
+
+	testCases := []struct {
+		name    string
+		fakeErr error
+	}{
+		{
+			name:    "set ingress policing successfully",
+			fakeErr: nil,
+		},
+		{
+			name:    "fail to set ingress policing",
+			fakeErr: errors.New("failed to set policing"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			execCommand := &FakeExecCommand{Err: tc.fakeErr}
+			SetExecCommand(execCommand)
+			result := setIngressPolicing(portName, rateBits, burstBits)
+			SetDefaultExecCommand()
+			assert.Equal(t, tc.fakeErr, result, "Unexpected result")
+			assert.Equal(t, expCmd, execCommand.Cmd, "Unexpected command executed")
+			assert.Equal(t, expArgs, execCommand.Args, "Unexpected command arguments")
+		})
+	}
+}
+
+func TestClearIngressPolicing(t *testing.T) {
+	expCmd := "ovs-vsctl"
+	portName := "test-port"
+	expArgs := []string{"set", "Interface", "test-port", "ingress_policing_rate=0", "ingress_policing_burst=0"}
+
+	testCases := []struct {
+		name    string
+		fakeErr error
+	}{
+		{
+			name:    "clear ingress policing successfully",
+			fakeErr: nil,
+		},
+		{
+			name:    "fail to clear ingress policing",
+			fakeErr: errors.New("failed to clear policing"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			execCommand := &FakeExecCommand{Err: tc.fakeErr}
+			SetExecCommand(execCommand)
+			result := clearIngressPolicing(portName)
+			SetDefaultExecCommand()
+			assert.Equal(t, tc.fakeErr, result, "Unexpected result")
+			assert.Equal(t, expCmd, execCommand.Cmd, "Unexpected command executed")
+			assert.Equal(t, expArgs, execCommand.Args, "Unexpected command arguments")
+		})
+	}
+}
+
+func TestSetEgressPolicer(t *testing.T) {
+	expCmd := "ovs-vsctl"
+	portName := "test-port"
+	rateBits := uint64(1000000000)  // 1 Gbps
+	burstBits := uint64(1000000000) // 1 Gb
+	// Expected conversion: bits/sec -> bytes/sec, bits -> bytes
+	expArgs := []string{
+		"set", "port", "test-port", "qos=@newqos", "--",
+		"--id=@newqos", "create", "qos", "type=egress-policer",
+		"other-config:cir=125000000", "other-config:cbs=125000000",
+	}
+
+	testCases := []struct {
+		name    string
+		fakeErr error
+	}{
+		{
+			name:    "set egress policer successfully",
+			fakeErr: nil,
+		},
+		{
+			name:    "fail to set egress policer",
+			fakeErr: errors.New("failed to set egress policer"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			execCommand := &FakeExecCommand{Err: tc.fakeErr}
+			SetExecCommand(execCommand)
+			result := setEgressPolicer(portName, rateBits, burstBits)
+			SetDefaultExecCommand()
+			assert.Equal(t, tc.fakeErr, result, "Unexpected result")
+			assert.Equal(t, expCmd, execCommand.Cmd, "Unexpected command executed")
+			assert.Equal(t, expArgs, execCommand.Args, "Unexpected command arguments")
+		})
+	}
+}
+
+func TestClearEgressPolicer(t *testing.T) {
+	expCmd := "ovs-vsctl"
+	portName := "test-port"
+	expArgs := []string{"--if-exists", "clear", "port", "test-port", "qos"}
+
+	testCases := []struct {
+		name    string
+		fakeErr error
+	}{
+		{
+			name:    "clear egress policer successfully",
+			fakeErr: nil,
+		},
+		{
+			name:    "fail to clear egress policer",
+			fakeErr: errors.New("failed to clear egress policer"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			execCommand := &FakeExecCommand{Err: tc.fakeErr}
+			SetExecCommand(execCommand)
+			result := clearEgressPolicer(portName)
+			SetDefaultExecCommand()
+			assert.Equal(t, tc.fakeErr, result, "Unexpected result")
+			assert.Equal(t, expCmd, execCommand.Cmd, "Unexpected command executed")
+			assert.Equal(t, expArgs, execCommand.Args, "Unexpected command arguments")
+		})
+	}
+}

cniovs/cniovs.go
@@ -356,6 +356,25 @@ func addLocalDeviceVhost(conf *types.NetConf, args *skel.CmdArgs, actualSharedDi
 		data.Socket = conf.HostConf.VhostConf.Socketfile
 		data.Vhostname = vhostName
 		data.IfMac = generateRandomMacAddress()
+
+		// Apply bandwidth/QoS settings if configured (best-effort, don't fail on errors)
+		// EgressRate/Burst limits traffic FROM the container (uses OVS ingress policing)
+		if conf.HostConf.BandwidthConf.EgressRate > 0 && conf.HostConf.BandwidthConf.EgressBurst > 0 {
+			if qosErr := setIngressPolicing(vhostName,
+				conf.HostConf.BandwidthConf.EgressRate,
+				conf.HostConf.BandwidthConf.EgressBurst); qosErr != nil {
+				logging.Errorf("Failed to set ingress policing (container egress QoS): %v", qosErr)
+			}
+		}
+
+		// IngressRate/Burst limits traffic TO the container (uses OVS egress policer)
+		if conf.HostConf.BandwidthConf.IngressRate > 0 && conf.HostConf.BandwidthConf.IngressBurst > 0 {
+			if qosErr := setEgressPolicer(vhostName,
+				conf.HostConf.BandwidthConf.IngressRate,
+				conf.HostConf.BandwidthConf.IngressBurst); qosErr != nil {
+				logging.Errorf("Failed to set egress policer (container ingress QoS): %v", qosErr)
+			}
+		}
 	} else {
 		return err
 	}
@@ -365,6 +384,10 @@ func addLocalDeviceVhost(conf *types.NetConf, args *skel.CmdArgs, actualSharedDi
 
 func delLocalDeviceVhost(conf *types.NetConf, args *skel.CmdArgs, data *OvsSavedData) error {
 	sharedDir := getShortSharedDir(data.SharedDir)
+
+	// Clear QoS settings before deleting port (best-effort, ignore errors)
+	_ = clearEgressPolicer(data.Vhostname)
+	_ = clearIngressPolicing(data.Vhostname)
 
 	// ovs-vsctl --if-exists del-port
 	err := deleteVhostPort(data.Vhostname, conf.HostConf.BridgeConf.BridgeName)

pkg/configdata/configdata_test.go
@@ -49,101 +49,101 @@ func TestSaveRemoteConfig(t *testing.T) {
 		{
 			name:    "save to pod vhostuser with host mode client and NetConf name",
 			netConf: &types.NetConf{Name: "Simple NetConf", HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", VhostConf: types.VhostConf{Mode: "client"}}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{}},"ipResult":{"dns":{}},"name":"Simple NetConf"}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}},"name":"Simple NetConf"}]`,
 		},
 		{
 			name:    "save to pod vhostuser with host mode server",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", VhostConf: types.VhostConf{Mode: "server"}}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"client"},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"client"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod vhostuser with host mode client",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", VhostConf: types.VhostConf{Mode: "client"}}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod vhostuser with no host mode",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser"}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"client"},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"client"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod vhostuser with both host and ContainerConf mode server",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", VhostConf: types.VhostConf{Mode: "server"}}, ContainerConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", VhostConf: types.VhostConf{Mode: "server"}}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"engine":"ovs-dpdk","iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"engine":"ovs-dpdk","iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod vhostuser with ContainerConf mode server and Socketfile override",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", VhostConf: types.VhostConf{Mode: "client", Socketfile: "vhostuser-hostconf.sock"}}, ContainerConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", VhostConf: types.VhostConf{Mode: "server", Socketfile: "vhostuser-containerconf.sock"}}},
 			// FIXME: possible bug - Socketfile from ContainerConf is overrided by value from HostConf!
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"engine":"ovs-dpdk","iftype":"vhostuser","memif":{},"vhost":{"mode":"server","socketfile":"vhostuser-hostconf.sock"},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"engine":"ovs-dpdk","iftype":"vhostuser","memif":{},"vhost":{"mode":"server","socketfile":"vhostuser-hostconf.sock"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 
 		{
 			name:    "save to pod without ContainerConf netType",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", NetType: "bridge", VhostConf: types.VhostConf{Mode: "client"}}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:     "save to pod with ipResult and without ContainerConf netType",
 			netConf:  &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", NetType: "bridge", VhostConf: types.VhostConf{Mode: "client"}}},
 			ipResult: &current.Result{Interfaces: []*current.Interface{{Name: "vlan0", Mac: "fe:ed:de:ad:be:ef"}}},
-			expJson:  `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"netType":"interface","vhost":{"mode":"server"},"bridge":{}},"ipResult":{"interfaces":[{"name":"vlan0","mac":"fe:ed:de:ad:be:ef"}],"dns":{}}}]`,
+			expJson:  `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"netType":"interface","vhost":{"mode":"server"},"bridge":{},"bandwidth":{}},"ipResult":{"interfaces":[{"name":"vlan0","mac":"fe:ed:de:ad:be:ef"}],"dns":{}}}]`,
 		},
 		{
 			name:     "save to pod with ipResult and with ContainerConf netType set",
 			netConf:  &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", NetType: "bridge", VhostConf: types.VhostConf{Mode: "client"}}, ContainerConf: types.UserSpaceConf{NetType: "bridge"}},
 			ipResult: &current.Result{Interfaces: []*current.Interface{{Name: "vlan0", Mac: "fe:ed:de:ad:be:ef"}}},
-			expJson:  `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"netType":"bridge","vhost":{"mode":"server"},"bridge":{}},"ipResult":{"interfaces":[{"name":"vlan0","mac":"fe:ed:de:ad:be:ef"}],"dns":{}}}]`,
+			expJson:  `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"netType":"bridge","vhost":{"mode":"server"},"bridge":{},"bandwidth":{}},"ipResult":{"interfaces":[{"name":"vlan0","mac":"fe:ed:de:ad:be:ef"}],"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod with ContainerConf ifType set",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", NetType: "bridge", VhostConf: types.VhostConf{Mode: "client"}}, ContainerConf: types.UserSpaceConf{IfType: "interface"}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"interface","memif":{}, "vhost":{},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"interface","memif":{}, "vhost":{},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod with ifType memif and no host role",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "memif"}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"master"},"vhost":{},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"master"},"vhost":{},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod with ifType memif and ContainerConf role master and socketfile override",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "memif", MemifConf: types.MemifConf{Role: "master", Socketfile: "memif-hostconf.sock"}}, ContainerConf: types.UserSpaceConf{IfType: "memif", MemifConf: types.MemifConf{Role: "master", Socketfile: "memif-memifconf.sock"}}},
 			// FIXME: possible bug - Socketfile from ContainerConf is overrided by value from HostConf!
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"master","socketfile":"memif-hostconf.sock"},"vhost":{},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"master","socketfile":"memif-hostconf.sock"},"vhost":{},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod with ifType memif and host role master",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "memif", MemifConf: types.MemifConf{Role: "master"}}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"slave"},"vhost":{},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"slave"},"vhost":{},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod with ifType memif and host role slave",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "memif", MemifConf: types.MemifConf{Role: "slave"}}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"master"},"vhost":{},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"master"},"vhost":{},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod with ifType memif and host role master and mode ip",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "memif", MemifConf: types.MemifConf{Role: "master", Mode: "ip"}}},
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"slave","mode":"ip"},"vhost":{},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"slave","mode":"ip"},"vhost":{},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:    "save to pod with ifType memif and ContainerConf role master and mode ethernet",
 			netConf: &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "memif", MemifConf: types.MemifConf{Role: "slave", Mode: "ip"}}, ContainerConf: types.UserSpaceConf{IfType: "memif", MemifConf: types.MemifConf{Role: "master", Mode: "ethernet"}}},
 
-			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"master","mode":"ethernet"},"vhost":{},"bridge":{}},"ipResult":{"dns":{}}}]`,
+			expJson: `[{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"memif","memif":{"role":"master","mode":"ethernet"},"vhost":{},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}]`,
 		},
 		{
 			name:     "save to file",
 			netConf:  &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", NetType: "bridge", VhostConf: types.VhostConf{Mode: "client"}}},
 			testType: "client_nil",
-			expJson:  `{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{}},"ipResult":{"dns":{}}}`,
+			expJson:  `{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}`,
 		},
 		{
 			name:      "save to file to newly created shared dir",
 			netConf:   &types.NetConf{HostConf: types.UserSpaceConf{Engine: "ovs-dpdk", IfType: "vhostuser", NetType: "bridge", VhostConf: types.VhostConf{Mode: "client"}}},
 			testType:  "client_nil",
 			brokenDir: "none",
-			expJson:   `{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{}},"ipResult":{"dns":{}}}`,
+			expJson:   `{"containerId":"#UUID#","ifName":"#ifName#","name":"","config":{"iftype":"vhostuser","memif":{},"vhost":{"mode":"server"},"bridge":{},"bandwidth":{}},"ipResult":{"dns":{}}}`,
 			expErr:    nil,
 		},
 		{

pkg/types/types.go
@@ -50,17 +50,25 @@ type BridgeConf struct {
 	VlanId     int    `json:"vlanId,omitempty"`     // Optional VLAN Id
 }
 
+type BandwidthConf struct {
+	IngressRate  uint64 `json:"ingressRate,omitempty"`  // bits per second (traffic to container)
+	IngressBurst uint64 `json:"ingressBurst,omitempty"` // bits (traffic to container)
+	EgressRate   uint64 `json:"egressRate,omitempty"`   // bits per second (traffic from container)
+	EgressBurst  uint64 `json:"egressBurst,omitempty"`  // bits (traffic from container)
+}
+
 type UserSpaceConf struct {
 	// The Container Instance will default to the Host Instance value if a given attribute
 	// is not provided. However, they are not required to be the same and a Container
 	// attribute can be provided to override. All values are listed as 'omitempty' to
 	// allow the Container struct to be empty where desired.
-	Engine     string     `json:"engine,omitempty"`  // CNI Implementation {vpp|ovs-dpdk}
-	IfType     string     `json:"iftype,omitempty"`  // Type of interface {memif|vhostuser}
-	NetType    string     `json:"netType,omitempty"` // Interface network type {none|bridge|interface}
-	MemifConf  MemifConf  `json:"memif,omitempty"`
-	VhostConf  VhostConf  `json:"vhost,omitempty"`
-	BridgeConf BridgeConf `json:"bridge,omitempty"`
+	Engine        string        `json:"engine,omitempty"`    // CNI Implementation {vpp|ovs-dpdk}
+	IfType        string        `json:"iftype,omitempty"`    // Type of interface {memif|vhostuser}
+	NetType       string        `json:"netType,omitempty"`   // Interface network type {none|bridge|interface}
+	MemifConf     MemifConf     `json:"memif,omitempty"`
+	VhostConf     VhostConf     `json:"vhost,omitempty"`
+	BridgeConf    BridgeConf    `json:"bridge,omitempty"`
+	BandwidthConf BandwidthConf `json:"bandwidth,omitempty"` // QoS/rate-limiting configuration
 }
 
 type NetConf struct {

userspace/cni/cni.go
@@ -67,6 +67,17 @@ func LoadNetConf(bytes []byte) (*types.NetConf, error) {
 	}
 
 	//
+	// Validate bandwidth configuration - rate and burst must be set together
+	//
+	bw := netconf.HostConf.BandwidthConf
+	if (bw.IngressRate > 0) != (bw.IngressBurst > 0) {
+		return nil, fmt.Errorf("bandwidth ingressRate and ingressBurst must both be set")
+	}
+	if (bw.EgressRate > 0) != (bw.EgressBurst > 0) {
+		return nil, fmt.Errorf("bandwidth egressRate and egressBurst must both be set")
+	}
+
+	//
 	// Parse previous result
 	//
 	/*

commit aa15e7d8cdcd70b23beace38287275c6c4aeb3ba

cniovs: Delete QoS record when clearing egress policer
The clearEgressPolicer function was only clearing the QoS reference
from the port, leaving orphaned QoS records in the OVS database.

Fix by first retrieving the QoS UUID attached to the port, then
destroying the QoS record and clearing the port reference in a
single atomic command.

Fixes: 984c2750ee83 ("cniovs: Add QoS/rate-limiting for vhost-user ports")

Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>

cniovs/ovsctrl.go
@@ -275,13 +275,26 @@ func setEgressPolicer(portName string, rateBits uint64, burstBits uint64) error 
 	return err
 }
 
-// clearEgressPolicer removes egress-policer QoS from an OVS port.
+// clearEgressPolicer removes egress-policer QoS from an OVS port and deletes the QoS record.
 func clearEgressPolicer(portName string) error {
-	// COMMAND: ovs-vsctl clear port <port> qos
-	// Note: This clears the QoS reference. Orphaned QoS objects can be garbage collected.
+	// First, get the QoS UUID attached to the port
 	cmd := "ovs-vsctl"
-	args := []string{"--if-exists", "clear", "port", portName, "qos"}
-	_, err := execCommand(cmd, args)
-	logging.Verbosef("ovsctl.clearEgressPolicer(): port=%s return=%v", portName, err)
+	args := []string{"--if-exists", "get", "Port", portName, "qos"}
+	output, err := execCommand(cmd, args)
+	if err != nil {
+		logging.Verbosef("ovsctl.clearEgressPolicer(): failed to get qos for port=%s: %v", portName, err)
+		return err
+	}
+
+	qosUUID := strings.TrimSpace(string(output))
+	if qosUUID == "" || qosUUID == "[]" {
+		logging.Verbosef("ovsctl.clearEgressPolicer(): port=%s has no QoS attached", portName)
+		return nil
+	}
+
+	// Destroy QoS record and clear reference from port in a single atomic command
+	args = []string{"--", "destroy", "QoS", qosUUID, "--", "clear", "Port", portName, "qos"}
+	_, err = execCommand(cmd, args)
+	logging.Verbosef("ovsctl.clearEgressPolicer(): port=%s qos=%s return=%v", portName, qosUUID, err)
 	return err
 }

cniovs/ovsctrl_test.go
@@ -551,30 +551,55 @@ func TestSetEgressPolicer(t *testing.T) {
 func TestClearEgressPolicer(t *testing.T) {
 	expCmd := "ovs-vsctl"
 	portName := "test-port"
-	expArgs := []string{"--if-exists", "clear", "port", "test-port", "qos"}
+	qosUUID := "12345678-1234-1234-1234-123456789abc"
+	expGetArgs := []string{"--if-exists", "get", "Port", "test-port", "qos"}
+	expDestroyArgs := []string{"--", "destroy", "QoS", qosUUID, "--", "clear", "Port", "test-port", "qos"}
 
 	testCases := []struct {
-		name    string
-		fakeErr error
+		name       string
+		fakeOut    []byte
+		fakeErr    error
+		expArgs    []string
+		expResult  error
 	}{
 		{
-			name:    "clear egress policer successfully",
-			fakeErr: nil,
+			name:      "clear egress policer successfully",
+			fakeOut:   []byte(qosUUID),
+			fakeErr:   nil,
+			expArgs:   expDestroyArgs,
+			expResult: nil,
 		},
 		{
-			name:    "fail to clear egress policer",
-			fakeErr: errors.New("failed to clear egress policer"),
+			name:      "no QoS attached - empty",
+			fakeOut:   []byte(""),
+			fakeErr:   nil,
+			expArgs:   expGetArgs,
+			expResult: nil,
+		},
+		{
+			name:      "no QoS attached - empty array",
+			fakeOut:   []byte("[]"),
+			fakeErr:   nil,
+			expArgs:   expGetArgs,
+			expResult: nil,
+		},
+		{
+			name:      "fail to get QoS",
+			fakeOut:   nil,
+			fakeErr:   errors.New("failed to get qos"),
+			expArgs:   expGetArgs,
+			expResult: errors.New("failed to get qos"),
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			execCommand := &FakeExecCommand{Err: tc.fakeErr}
+			execCommand := &FakeExecCommand{Out: tc.fakeOut, Err: tc.fakeErr}
 			SetExecCommand(execCommand)
 			result := clearEgressPolicer(portName)
 			SetDefaultExecCommand()
-			assert.Equal(t, tc.fakeErr, result, "Unexpected result")
+			assert.Equal(t, tc.expResult, result, "Unexpected result")
 			assert.Equal(t, expCmd, execCommand.Cmd, "Unexpected command executed")
-			assert.Equal(t, expArgs, execCommand.Args, "Unexpected command arguments")
+			assert.Equal(t, tc.expArgs, execCommand.Args, "Unexpected command arguments")
 		})
 	}
 }